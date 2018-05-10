24f9001a0069b4c0a9fe3b4052aca1a2c18b6f83
Вадим Гатауллин <Vadya@MacBook-Pro-Vadim-2.local>
2018-05-10 21:53:36 +0300

    Node Jdoc

--- src/sample/models/Node.java
+++ src/sample/models/Node.java
@@ -1,30 +1,30 @@
 package sample.models;
 
 /**
- *
+ * Класс узла дерева
  */
 public class Node {
     /**
-     *
+     * Правый ребенок
      */
     public Node right;
     /**
-     *
+     * Левый ребенок
      */
     public Node left;
     /**
-     *
+     * Ключ-отрезок узла
      */
     public Line key;
     /**
-     *
+     * Дистанция, по которой сортировка в BST происходит
      */
     public double d;
 
     /**
-     *
-     * @param l
-     * @param dist
+     * Конструктор узла
+     * @param l Ключ-отрезок
+     * @param dist Дистанция
      */
     public Node(Line l, double dist)
     {
